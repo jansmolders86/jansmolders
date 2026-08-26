44a8830a66ba89414fe147a7e2bda975ed7f813f
Jan Smolders <jansmolders86@gmail.com>
2026-08-26 14:30:26 +0200

    Add files via upload

--- karaoke/config.js
+++ karaoke/config.js
@@ -1,12 +1,12 @@
 // Vul dit EENMALIG in voordat je de map uploadt.
 // Client ID uit jouw Spotify Developer Dashboard:
 window.KARAOKE_CONFIG = {
-  spotifyClientId: "PLAK_HIER_JE_CLIENT_ID",
+  spotifyClientId: "20d0f2fae3a242d5bc5837842e5992e3",
 
   // Plak hier de Spotify playlist-URL of URI van "Student Karaoke 2026 🎤".
   // Voorbeeld URL: https://open.spotify.com/playlist/abc123
   // Voorbeeld URI: spotify:playlist:abc123
-  playlist: "PLAK_HIER_DE_PLAYLIST_URL_OF_URI",
+  playlist: "https://open.spotify.com/playlist/01yy00Ewts0uV3XDFLzKx1",
 
   playlistName: "Student Karaoke 2026 🎤"
 };
